2c00fb17ea72e530600392baaa9aaaf575c775ec
Akaramsetty <2398113@cognizant.com>
2025-07-25 22:11:56 +0530

    Updated Spring Boot

--- src/main/java/com/example/demo/Controller/Controller.java
+++ src/main/java/com/example/demo/Controller/Controller.java
@@ -1,12 +1,29 @@
 package com.example.demo.Controller;
-
+import com.example.demo.Model.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+import java.util.HashMap;
 @RestController
 public class Controller {
     @GetMapping("/")
-    public String add()
+    public Map<String,Object> add()
     {
-        return "hello world";
+        Map<String,Object> response=new HashMap<>();
+        response.put("name","mahesh");
+        return response;
     }
+   @PostMapping("/post")
+public Map<String, Object> sub(@RequestBody Model s) {
+    Map<String, Object> response = new HashMap<>();
+    System.out.println(s);
+    response.put("status", "success");
+    response.put("id",s.getId());
+    response.put("name",s.getName());
+    response.put("gender",s.getGender());
+    return response;
+}
+
 }
---
+++ src/main/java/com/example/demo/Model/Model.java
@@ -0,0 +1,20 @@
+package com.example.demo.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Model {
+    private long id;
+    private String name;
+    private String gender;
+    public Model(long id, String name, String gender) {
+        this.id = id;
+                this.gender = gender;
+
+        this.name = name;
+    }
+   
+
+}
